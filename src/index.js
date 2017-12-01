f34e72adbc7198603f6f7cc409a181b4300d019e
Pim Brouwers <pbrouwers@nhlpa.com>
2017-12-01 13:54:27 -0500

    its alive!

--- src/index.js
+++ src/index.js
@@ -1,76 +1,163 @@
 /*
  * Ditto Handlebars Middleware
  */
-const 
-	events = require('events'),
-	fs = require('fs'),
-	hbs = require('handlebars'),
-	path = require('path'),
-	util = require('util');
+const
+  events = require('events'),
+  fs = require('fs'),
+  glob = require('glob'),
+  hbs = require('handlebars'),
+  path = require('path'),
+  util = require('util');
 
 module.exports = DittoHbs;
 
 function DittoHbs(opt) {
-	events.EventEmitter.call(this);
+  events.EventEmitter.call(this);
 
-	opt = opt || {};
-
-	return function(files, Ditto, done){
-		this.run(files, Ditto, done);
+  this.opt = opt || {
+		defaultTemplate: 'index',
+  	partials: './templates/partials',
+  	templates: './templates'
 	};
+  this.files = {};
+	this.templates = {};
 };
 
 /* Inherit Event Emitter prototype */
 util.inherits(DittoHbs, events.EventEmitter);
 
 /* Run */
-DittoHbs.prototype.run = function(files, Ditto, done){
-	console.log("das");
+DittoHbs.prototype.run = function(files, Ditto, done) {
+  this.files = files;
+
+  //register listeners
+  this.on("foundPartials", this.registerPartials);
+  this.on("registeredPartials", this.discoverTemplates);
+  this.on("foundTemplates", this.compileTemplates);
+	this.on("compiledTemplates", this.renderHtml);
+	this.on("renderHtmlComplete", done);
+
+  try {
+    //kickoff build
+    this.discoverPartials();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
-/* Register Partials */
-DittoHbs.prototype.registerPartials = function(opt){
-	var 
-		self = this,
-		partialsDir = path.resolve(opt.partials),
-		promises = [];		
-		
-	if(opt.partials)
-	{
-		var partialsDir = path.resolve(opt.partials);		
-		
-		fs.readdir(partialsDir, function(err, filepaths){				
-			if(err) throw err;
+/* Compile Pages */
+DittoHbs.prototype.compileTemplates = function(filepaths) {
+	console.info("INFO: compileTemplates()");
 
-			filepaths.map(function(filepath){					
-				if (path.extname(filepath) === '.hbs')
-				{
-					promises.push(self.registerPartial(filepath));
-				}					
-			});
-		});
-	}
+  var self = this,
+	    templatesDir = path.resolve(self.opt.templates),
+	    promises = [];
 
-	Promise.all(promises)
-    .then(function(){
-      self.emit("registeredPartials");
+  filepaths.map(function(filepath) {
+    promises.push(self.compileTemplate(path.resolve(templatesDir, filepath)));
+  });
+
+  Promise.all(promises)
+    .then(function() {
+      self.emit("compiledTemplates");
     })
-    .catch(function(reason){
+		.catch(function(reason) {
+			console.error(reason);
       throw new Error(reason);
     });
 };
 
-DittoHbs.prototype.registerPartial = function(filepath){
-	return new Promise(function(resolve, reject){
-		fs.readFile(filepath, function(err, data){							
-			if(err) {
-				console.error(err);
-				reject(err);
-			}
-			else {
-				hbs.registerPartial(path.parse(filepath).name, data);
-				resolve();
-			}			
-		});		
+/* Compile Template Async */
+DittoHbs.prototype.compileTemplate = function(filepath) {
+	var self = this;
+
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filepath, 'utf8', function(err, data) {
+      if (err) reject(err);
+      else {
+				self.templates[path.parse(filepath).name] = hbs.compile(data);
+        resolve();
+      }
+    });
+  });
+};
+
+/* Discover Partials */
+DittoHbs.prototype.discoverPartials = function() {
+  console.info("INFO: discoverPartials()");
+
+  var self = this,
+    partialsDir = path.resolve(self.opt.partials);
+
+  glob(path.join(partialsDir, '/*.hbs'), function(err, filepaths) {
+    self.emit("foundPartials", filepaths);
+  });
+};
+
+/* Discover Partials */
+DittoHbs.prototype.discoverTemplates = function() {
+  console.info("INFO: discoverPartials()");
+
+  var self = this,
+    templatesDir = path.resolve(self.opt.templates);
+
+  glob(path.join(templatesDir, '/*.hbs'), function(err, filepaths) {
+    self.emit("foundTemplates", filepaths);
+  });
+};
+
+/* Register Partials */
+DittoHbs.prototype.registerPartials = function(filepaths) {
+  console.info("INFO: registerPartials()");
+
+  var self = this,
+    partialsDir = path.resolve(self.opt.partials),
+    promises = [];
+
+  filepaths.map(function(filepath) {
+    promises.push(self.registerPartial(path.resolve(partialsDir, filepath)));
+  });
+
+  Promise.all(promises)
+    .then(function() {
+      self.emit("registeredPartials");
+    })
+		.catch(function(reason) {
+			console.error(reason);
+      throw new Error(reason);
+    });
+};
+
+/* Register Partial Async */
+DittoHbs.prototype.registerPartial = function(filepath) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filepath, 'utf8', function(err, data) {
+      if (err) reject(err);
+      else {
+        hbs.registerPartial(path.parse(filepath).name, data);
+        resolve();
+      }
+    });
+  });
+};
+
+/* Render HTML */
+DittoHbs.prototype.renderHtml = function () {
+	var self = this;
+
+	Object.keys(self.files).forEach(function(filepath){
+		var file = self.files[filepath],
+				parsedPath = path.parse(filepath),
+				tmpl = file.template || self.opt.defaultTemplate,
+				html = self.templates[tmpl](file.content),
+				newFileDotPath = (parsedPath.name !== "index") ?
+														path.join(parsedPath.dir, parsedPath.name, "index.html") :
+														path.join(parsedPath.dir, parsedPath.name + ".html");
+		
+		file.content = html;
+		file.path = newFileDotPath;
 	});
+
+	self.emit("renderHtmlComplete");
 };

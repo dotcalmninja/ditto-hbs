3a5792ef8b4ef820880cccb9a0571b1a4061c5de
Pim Brouwers <mpbrouwers@gmail.com>
2017-12-05 15:59:54 -0500

    hbs extension removal on renderHtml

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@
  * Ditto Handlebars Middleware
  */
 const
-  events = require('events'),
-  fs = require('fs'),
-  glob = require('glob'),
-  hbs = require('handlebars'),
-  path = require('path'),
-  util = require('util');
+    events = require('events'),
+    fs = require('fs'),
+    glob = require('glob'),
+    hbs = require('handlebars'),
+    path = require('path'),
+    util = require('util');
 
 module.exports = DittoHbs;
 
 function DittoHbs(opt) {
-  this.opt = opt || {
-		defaultTemplate: 'index',
-  	partials: './templates/partials',
-  	templates: './templates'
-	};
-  this.files = {};
-  this.metadata = {};
-	this.templates = {};
+    this.opt = opt || {
+        defaultTemplate: 'index',
+        partials: './templates/partials',
+        templates: './templates'
+    };
+    this.files = {};
+    this.metadata = {};
+    this.templates = {};
 };
 
 /* Inherit Event Emitter prototype */
@@ -27,140 +27,143 @@ util.inherits(DittoHbs, events.EventEmitter);
 
 /* Run */
 DittoHbs.prototype.run = function(files, Ditto, done) {
-  this.files = files;
-  this.metadata = Ditto._metadata;
+    this.files = files;
+    this.metadata = Ditto._metadata;
 
-  //register listeners
-  this.on("foundPartials", this.registerPartials);
-  this.on("registeredPartials", this.discoverTemplates);
-  this.on("foundTemplates", this.compileTemplates);
-  this.on("compiledTemplates", this.renderHtml);
-  this.on("renderHtmlComplete", done);
+    //register listeners
+    this.on("foundPartials", this.registerPartials);
+    this.on("registeredPartials", this.discoverTemplates);
+    this.on("foundTemplates", this.compileTemplates);
+    this.on("compiledTemplates", this.renderHtml);
+    this.on("renderHtmlComplete", done);
 
-  try {
-    //kickoff build
-    this.discoverPartials();
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+    try {
+        //kickoff build
+        this.discoverPartials();
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 };
 
 /* Compile Pages */
 DittoHbs.prototype.compileTemplates = function(filepaths) {
-	console.info("INFO: compileTemplates()");
+    console.info("INFO: compileTemplates()");
 
-  var self = this,
-	    templatesDir = path.resolve(self.opt.templates),
-	    promises = [];
+    var self = this,
+        templatesDir = path.resolve(self.opt.templates),
+        promises = [];
 
-  filepaths.map(function(filepath) {
-    promises.push(self.compileTemplate(path.resolve(templatesDir, filepath)));
-  });
-
-  Promise.all(promises)
-    .then(function() {
-      self.emit("compiledTemplates");
-    })
-		.catch(function(reason) {
-			console.error(reason);
-      throw new Error(reason);
+    filepaths.map(function(filepath) {
+        promises.push(self.compileTemplate(path.resolve(templatesDir, filepath)));
     });
+
+    Promise.all(promises)
+        .then(function() {
+            self.emit("compiledTemplates");
+        })
+        .catch(function(reason) {
+            console.error(reason);
+            throw new Error(reason);
+        });
 };
 
 /* Compile Template Async */
 DittoHbs.prototype.compileTemplate = function(filepath) {
-	var self = this;
+    var self = this;
 
-  return new Promise(function(resolve, reject) {
-    fs.readFile(filepath, 'utf8', function(err, data) {
-      if (err) reject(err);
-      else {
-				self.templates[path.parse(filepath).name] = hbs.compile(data);
-        resolve();
-      }
+    return new Promise(function(resolve, reject) {
+        fs.readFile(filepath, 'utf8', function(err, data) {
+            if (err) reject(err);
+            else {
+                self.templates[path.parse(filepath).name] = hbs.compile(data);
+                resolve();
+            }
+        });
     });
-  });
 };
 
 /* Discover Partials */
 DittoHbs.prototype.discoverPartials = function() {
-  console.info("INFO: discoverPartials()");
+    console.info("INFO: discoverPartials()");
 
-  var self = this,
-    partialsDir = path.resolve(self.opt.partials);
+    var self = this,
+        partialsDir = path.resolve(self.opt.partials);
 
-  glob(path.join(partialsDir, '/*.hbs'), function(err, filepaths) {
-    self.emit("foundPartials", filepaths);
-  });
+    glob(path.join(partialsDir, '/*.hbs'), function(err, filepaths) {
+        self.emit("foundPartials", filepaths);
+    });
 };
 
 /* Discover Partials */
 DittoHbs.prototype.discoverTemplates = function() {
-  console.info("INFO: discoverPartials()");
+    console.info("INFO: discoverPartials()");
 
-  var self = this,
-    templatesDir = path.resolve(self.opt.templates);
+    var self = this,
+        templatesDir = path.resolve(self.opt.templates);
 
-  glob(path.join(templatesDir, '/*.hbs'), function(err, filepaths) {
-    self.emit("foundTemplates", filepaths);
-  });
+    glob(path.join(templatesDir, '/*.hbs'), function(err, filepaths) {
+        self.emit("foundTemplates", filepaths);
+    });
 };
 
 /* Register Partials */
 DittoHbs.prototype.registerPartials = function(filepaths) {
-  console.info("INFO: registerPartials()");
+    console.info("INFO: registerPartials()");
 
-  var self = this,
-    partialsDir = path.resolve(self.opt.partials),
-    promises = [];
+    var self = this,
+        partialsDir = path.resolve(self.opt.partials),
+        promises = [];
 
-  filepaths.map(function(filepath) {
-    promises.push(self.registerPartial(path.resolve(partialsDir, filepath)));
-  });
-
-  Promise.all(promises)
-    .then(function() {
-      self.emit("registeredPartials");
-    })
-		.catch(function(reason) {
-			console.error(reason);
-      throw new Error(reason);
+    filepaths.map(function(filepath) {
+        promises.push(self.registerPartial(path.resolve(partialsDir, filepath)));
     });
+
+    Promise.all(promises)
+        .then(function() {
+            self.emit("registeredPartials");
+        })
+        .catch(function(reason) {
+            console.error(reason);
+            throw new Error(reason);
+        });
 };
 
 /* Register Partial Async */
 DittoHbs.prototype.registerPartial = function(filepath) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(filepath, 'utf8', function(err, data) {
-      if (err) reject(err);
-      else {
-        hbs.registerPartial(path.parse(filepath).name, data);
-        resolve();
-      }
+    return new Promise(function(resolve, reject) {
+        fs.readFile(filepath, 'utf8', function(err, data) {
+            if (err) reject(err);
+            else {
+                hbs.registerPartial(path.parse(filepath).name, data);
+                resolve();
+            }
+        });
     });
-  });
 };
 
 /* Render HTML */
-DittoHbs.prototype.renderHtml = function () {
-	var self = this;
+DittoHbs.prototype.renderHtml = function() {
+    var self = this;
 
-	Object.keys(self.files).forEach(function(filepath){
-		var file = self.files[filepath],
-				parsedPath = path.parse(filepath),
-				tmpl = file.template || self.opt.defaultTemplate,
-				newFileDotPath = (parsedPath.name !== "index") ?
-														path.join(parsedPath.dir, parsedPath.name, "index.html") :
-														path.join(parsedPath.dir, parsedPath.name + ".html");
+    Object.keys(self.files).forEach(function(filepath) {
+        var file = self.files[filepath],
+            parsedPath = path.parse(filepath),
+            tmpl = file.template || self.opt.defaultTemplate,
+            newFileDotPath = (parsedPath.name !== "index") ?
+            path.join(parsedPath.dir, parsedPath.name, "index.html") :
+            path.join(parsedPath.dir, parsedPath.name + ".html");
 
-    var pageData = file.content;
+        //strip extension if its there
+        tmpl = path.parse(tmpl).name;
 
-    if(self.metadata) pageData.metadata = self.metadata;
+        var pageData = file.content;
 
-		file.content = self.templates[tmpl](file.content);
-		file.path = newFileDotPath;
-	});
+        if (self.metadata) pageData.metadata = self.metadata;
 
-	self.emit("renderHtmlComplete");
+        file.content = self.templates[tmpl](file.content);
+        file.path = newFileDotPath;
+    });
+
+    self.emit("renderHtmlComplete");
 };

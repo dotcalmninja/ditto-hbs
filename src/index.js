f5c660ade73a7efb89e8a8226b57904d20e1a737
Pim Brouwers <pbrouwers@nhlpa.com>
2018-08-10 17:30:51 -0400

    run() removed from prototype

--- src/index.js
+++ src/index.js
@@ -13,129 +13,128 @@ const
 module.exports = dittoHbs;
 
 function dittoHbs(opt) {
-  this.opt = new dittoHbsOpt(opt);
+  opt = new dittoHbsOpt(opt);
+
+  /**
+   * Discover files for given file pattern
+   * @param {String} pattern glob pattern
+   * @param {Function.<Error, Array.<string>>} callback
+   */
+  function discover(pattern, callback) {
+    glob(pattern, function (err, filepaths) {
+      if (err) callback(err);
+      callback(null, filepaths);
+    });
+  };
+
+  /**
+   * Discover files in partials directory
+   * @param {Function.<Error, Array.<string>>} callback
+   */
+  function discoverPartials(callback) {
+    discover(path.join(opt.partials, '/*.hbs'), callback)
+  };
+
+  /**
+   * Discover files in templates directory
+   * @param {Function.<Error, Array.<string>>} callback
+   */
+  function discoverTemplates(callback) {
+    discover(path.join(opt.templates, '/*.hbs'), callback);
+  };
+
+  /**
+   * Read hbs partial files
+   * @param {Array.<String>} filepaths 
+   * @param {Function.<Error>} callback
+   */
+  function registerPartials(filepaths, callback) {
+    async.map(filepaths, registerPartial, function (err) {
+      if (err) callback(err);
+      callback(null);
+    });
+  };
+
+  /**
+   * Read file into buffer and register as hbs partials
+   * @param {String} filepath 
+   * @param {Function.<Error>} callback
+   */
+  function registerPartial(filepath, callback) {
+    fs.readFile(filepath, 'utf8', function (err, data) {
+      if (err) callback(err);
+      hbs.registerPartial(path.parse(filepath).name, data);
+      callback(null);
+    });
+  };
+
+  /**
+   * Read hbs template files
+   * @param {Array.<String>} filepaths 
+   * @param {Function.<Error>} callback
+   */
+  function registerTemplates(filepaths, callback) {
+    async.map(filepaths, registerTemplate, function (err, templates) {
+      if (err) callback(err);
+      callback(null, templates);
+    });
+  };
+
+  /**
+   * Read file into buffer and register as hbs template
+   * @param {String} filepath 
+   * @param {Function.<Error, Object>} callback
+   */
+  function registerTemplate(filepath, callback) {
+    fs.readFile(filepath, 'utf8', function (err, data) {
+      if (err) callback(err);
+      callback(null, {
+        name: path.parse(filepath).name,
+        template: hbs.compile(data)
+      });
+    });
+  };
+
+  /**
+   * @param {Array.<Object.<dittoFile>>} files 
+   * @param {Object.<ditto>} ditto 
+   * @param {Array.<Object>} templates
+   * @param {Function.<Error>} callback
+   */
+  function renderHtml(files, ditto, templates, callback) {
+    files.forEach(function (file) {
+      //update the file extension  we're writing rendering html now
+      file.path.ext = '.html';
+
+      //build up array of potential templates
+      let potentialTemplateNames = [file.path.name, file.path.dir.replace('/', '-'), opt.defaultTemplate];
+
+      //resolve template
+      potentialTemplateNames.some(function (potentialTemplateName) {
+        var tmpl = templates.find(function (template) { return template.name == potentialTemplateName });
+
+        if (tmpl) {
+          //render
+          file.content.metadata = ditto._metadata;
+          file.content = tmpl.template(file.content);
+          return true;
+        }
+      });
+    });
+
+    callback(null, files);
+  };
 
   return function (files, ditto, done) {
     async.waterfall([
-      this.discoverPartials.bind(this),
-      this.registerPartials.bind(this),
-      this.discoverTemplates.bind(this),
-      this.registerTemplates.bind(this),
-      this.renderHtml.bind(this, files, ditto)
+      discoverPartials,
+      registerPartials,
+      discoverTemplates,
+      registerTemplates,
+      renderHtml.bind(this, files, ditto)
     ], function (err, files) {
       done(err, files);
     });
   };
 };
 
-/**
- * Discover files for given file pattern
- * @param {String} pattern glob pattern
- * @param {Function.<Error, Array.<string>>} callback
- */
-dittoHbs.prototype.discover = function(pattern, callback) {
-  glob(pattern, function(err, filepaths) {
-    if (err) callback(err);
-    callback(null, filepaths);
-  });
-};
-
-/**
- * Discover files in partials directory
- * @param {Function.<Error, Array.<string>>} callback
- */
-dittoHbs.prototype.discoverPartials = function(callback) {
-  this.discover(path.join(this.opt.partials, '/*.hbs'), callback)
-};
-
-/**
- * Discover files in templates directory
- * @param {Function.<Error, Array.<string>>} callback
- */
-dittoHbs.prototype.discoverTemplates = function(callback) {
-  this.discover(path.join(this.opt.templates, '/*.hbs'), callback);
-};
-
-/**
- * Read hbs partial files
- * @param {Array.<String>} filepaths 
- * @param {Function.<Error>} callback
- */
-dittoHbs.prototype.registerPartials = function(filepaths, callback) {
-  async.map(filepaths, this.registerPartial, function(err) {
-    if (err) callback(err);
-    callback(null);
-  });
-};
-
-/**
- * Read file into buffer and register as hbs partials
- * @param {String} filepath 
- * @param {Function.<Error>} callback
- */
-dittoHbs.prototype.registerPartial = function(filepath, callback) {
-  fs.readFile(filepath, 'utf8', function(err, data) {
-    if (err) callback(err);
-    hbs.registerPartial(path.parse(filepath).name, data);
-    callback(null);
-  });
-};
-
-/**
- * Read hbs template files
- * @param {Array.<String>} filepaths 
- * @param {Function.<Error>} callback
- */
-dittoHbs.prototype.registerTemplates = function(filepaths, callback) {
-  async.map(filepaths, this.registerTemplate, function(err, templates) {
-    if (err) callback(err);
-    callback(null, templates);
-  });
-};
-
-/**
- * Read file into buffer and register as hbs template
- * @param {String} filepath 
- * @param {Function.<Error, Object>} callback
- */
-dittoHbs.prototype.registerTemplate = function(filepath, callback) {
-  fs.readFile(filepath, 'utf8', function(err, data) {
-    if (err) callback(err);
-    callback(null, {
-      name: path.parse(filepath).name,
-      template: hbs.compile(data)
-    });
-  });
-};
-
-/**
- * @param {Array.<Object.<DittoFile>>} files 
- * @param {Object.<Ditto>} Ditto 
- * @param {Array.<Object>} templates
- * @param {Function.<Error>} callback
- */
-dittoHbs.prototype.renderHtml = function(files, Ditto, templates, callback) {
-  let self = this;
-
-  files.forEach(function(file) {
-    //update the file extension  we're writing rendering html now
-    file.path.ext = '.html';
-
-    //build up array of potential templates
-    let potentialTemplateNames = [file.path.name, file.path.dir.replace('/', '-'), self.opt.defaultTemplate];
-    
-    //resolve template
-    potentialTemplateNames.some(function(potentialTemplateName){
-      var tmpl = templates.find(function(template){ return template.name == potentialTemplateName });
-      
-      if(tmpl){
-        //render
-        file.content.metadata = Ditto._metadata;
-        file.content = tmpl.template(file.content);        
-        return true;
-      }
-    });
-  });
-
-  callback(null, files);
-};
